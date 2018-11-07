c9314914f0c1321e35dae0578afa05c5710b465a
Tr1nks <Tr1nks96@gmail.com>
2018-11-07 22:24:28 +0200

    lr-2.1

---
+++ src/ua/edu/hneu/oop/java/lr2/task1/MainClass.java
@@ -0,0 +1,50 @@
+package ua.edu.hneu.oop.java.lr2.task1;
+
+
+import ua.edu.hneu.oop.java.util.ConsoleUtil;
+
+import java.io.IOException;
+
+public class MainClass {
+    private static final String EXIT_COMMAND = "\\exit";
+    private static final String INVITATION = "Enter phrase for de-verbization, or type \"\\exit\" for exit.";
+    private static final String IO_EXCEPTION_FORMAT = "Something went wrong with obtaining value. Original error is:\"%s\".%n%n";
+
+    private static ConsoleUtil consoleUtil;
+    private static VerbCutter verbCutter;
+
+    public static void main(String[] args) {
+        boolean run = true;
+        try {
+            init();
+        } catch (IOException e) {
+            consoleUtil.printf(IO_EXCEPTION_FORMAT, e.getMessage());
+            run = false;
+        }
+        do {
+            try {
+                consoleUtil.println(INVITATION);
+                String input = consoleUtil.readInput();
+                if (checkIfExit(input)) {
+                    break;
+                }
+                consoleUtil.println(verbCutter.cutVerbs(input));
+            } catch (IOException e) {
+                consoleUtil.printf(IO_EXCEPTION_FORMAT, e.getMessage());
+                run = false;
+            }
+
+        } while (run);
+
+    }
+
+    private static boolean checkIfExit(String input) {
+        return input.toLowerCase().contains(EXIT_COMMAND);
+    }
+
+    private static void init() throws IOException {
+        consoleUtil = new ConsoleUtil();
+        verbCutter = new VerbCutter();
+    }
+}
+
---
+++ src/ua/edu/hneu/oop/java/lr2/task1/VerbCutter.java
@@ -0,0 +1,39 @@
+package ua.edu.hneu.oop.java.lr2.task1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Pattern;
+
+public class VerbCutter {
+    private static final String VERB_PATTERN_FORMAT = "(?muU)(?<=\\W)(\\w)*(%s)(?=\\W)";
+    private static final String ENDINGS_FILE_NAME = "/verb-endings";
+
+    private static List<String> endings;
+    private static Pattern verbPattern;
+
+    public VerbCutter() throws IOException {
+        if (Objects.isNull(endings)) {
+            endings = loadEndings();
+        }
+    }
+
+
+    public String cutVerbs(String input) {
+        return input.replaceAll(String.format(VERB_PATTERN_FORMAT, String.join("|", endings)), "")
+                .replaceAll(" {2,}", " ");
+    }
+
+    private List<String> loadEndings() throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream(ENDINGS_FILE_NAME)))) {
+            List<String> loaded = new ArrayList<>();
+            for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+                loaded.add(line.trim());
+            }
+            return loaded;
+        }
+    }
+}
